4c9f069dff259d971dd846c14202c4506f655fe7
Cory House <Cory.House@coxautoinc.com>
2017-12-10 09:25:41 -0600

    Add homepage

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Pluralsight Administration</h1>
+        <p>React, MobX and React Router for ultra-responsive web apps.</p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
